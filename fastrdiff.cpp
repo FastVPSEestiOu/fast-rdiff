ca2d3a77af95bda721ffaee8b2dd537fdc9979fb
Pavel Odintsov <odintsov@fastvps.ru>
2014-10-04 20:37:10 +0400

    Fix parser

--- fastrdiff.cpp
+++ fastrdiff.cpp
@@ -3,6 +3,7 @@
 #include <sys/fcntl.h>
 #include <stdio.h>
 #include <endian.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -10,8 +11,12 @@ using namespace std;
 int read_int(int file_handle, int32_t* int_ptr) {
     int32_t integer_value = 0;
 
-    read(file_handle, (char*)&integer_value, sizeof(integer_value));
+    int read_result = read(file_handle, (char*)&integer_value, sizeof(integer_value));
   
+    if (read_result <= 0) {
+	return 0;
+    }
+
     // convert big endian to little endian 
     integer_value = be32toh(integer_value);
 
@@ -54,17 +59,40 @@ bool read_signature_file() {
     read_int(file_handle, &md4_truncation_from_file);
 
     if (md4_truncation_from_file < 1 or md4_truncation_from_file > 16) {
-	cout<<"Trunkation is: "<<md4_truncation_from_file<<endl;
+	cout<<"Truncation is: "<<md4_truncation_from_file<<endl;
 	return false;
     }
 
     cout<<"Truncation for md4 is: "<<md4_truncation_from_file<<endl;
-   
-    while (true) {
-	uint32_t weak_checksumm = 0;
-	break;	
-	//read(file_handle, (char*)&weak_checksumm, sizeof(weak_checksumm));	
+  
+    char* md4_checksumm_buffer = (char*)malloc(sizeof(uint32_t) * md4_truncation_from_file);
+
+    if (!md4_checksumm_buffer) {
+	std::cout<<"Can't allocate buffer"<<endl;
+	return false;
     }
+
+    long long unsigned int offset = 0;
+    while (true) {
+	int32_t weak_checksumm = 0;
+
+	int read_result = read_int(file_handle, &weak_checksumm);
+
+	if (!read_result) {
+	    break;
+	}
+
+	read_result = read(file_handle, md4_checksumm_buffer, md4_truncation_from_file);
+	if (read_result <= 0) {
+	    break;
+	}
+
+	printf("weak: %x offset: %lld\n", weak_checksumm, offset);
+	// append_list(weak_checksumm, md4_checksumm_buffer, offset)
+	offset += blocksize_from_file; 
+    }
+
+    free(md4_checksumm_buffer);
  
     // rollsum, md4 signature, offset
     return true;
