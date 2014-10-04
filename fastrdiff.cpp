1f1a1976f348db396b95e60b675277a7ef58c4d2
Pavel Odintsov <odintsov@fastvps.ru>
2014-10-04 23:16:25 +0400

    Add persistent storage

--- fastrdiff.cpp
+++ fastrdiff.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
+
 #include <stdint.h>
 #include <sys/fcntl.h>
 #include <stdio.h>
 #include <endian.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 
 using namespace std;
 
@@ -19,6 +22,14 @@ void hexlify(const char* in, unsigned int size, char* out);
 void print_md4_summ(char* weak_checksumm, int md4_truncation_length);
 int read_int(int file_handle, int32_t* int_ptr);
 bool read_signature_file();
+
+typedef struct signature_element { 
+    char md4_checksumm[8];
+    int32_t weak_checksumm;
+    unsigned long long offset; 
+} signature_element;
+
+vector<signature_element> signatures_vector;
 
 int main() {
     read_signature_file();
@@ -64,14 +75,22 @@ bool read_signature_file() {
 	return false;
     }
 
-    cout<<"Truncation for md4 is: "<<md4_truncation_from_file<<endl;
-  
-    char* md4_checksumm_buffer = (char*)malloc(md4_truncation_from_file);
-
-    if (!md4_checksumm_buffer) {
-	std::cout<<"Can't allocate buffer"<<endl;
+    if (md4_truncation_from_file != 8) {
+	cout<<"We support only 8byte md4 truncation! Sorry :("<<endl;
 	return false;
     }
+
+    cout<<"Truncation for md4 is: "<<md4_truncation_from_file<<endl;
+  
+    //char* md4_checksumm_buffer = (char*)malloc(md4_truncation_from_file);
+
+    // TODO: пока не ясно как сделать поддержку динамического размера, но я не думаю, что она вообще кому-то нужна
+    char md4_checksumm_buffer[8];
+
+    //if (!md4_checksumm_buffer) {
+    //	std::cout<<"Can't allocate buffer"<<endl;
+    //	return false;
+    //}
 
     unsigned long long file_size = get_file_size(file_path.c_str());
 
@@ -80,6 +99,8 @@ bool read_signature_file() {
     unsigned long long signatures_count = int ( (file_size - sizeof(uint32_t) * 3) / (sizeof(uint32_t) + md4_truncation_from_file));
 
     std::cout<<"We calculated approximate signatures number as: "<<signatures_count<<endl;
+    // Для ускорения работы с вектором, мы резервируем место под ожидаемое число записей сразу 
+    signatures_vector.reserve(signatures_count);
 
     long long unsigned int offset = 0;
     while (true) {
@@ -100,12 +121,16 @@ bool read_signature_file() {
 	printf("md4: ");
 	print_md4_summ(md4_checksumm_buffer, md4_truncation_from_file);
 	printf("\n");
-	// append_list(weak_checksumm, md4_checksumm_buffer, offset)
+
+	signature_element current_element;
+	strncpy(current_element.md4_checksumm, md4_checksumm_buffer, 8);
+	current_element.weak_checksumm = weak_checksumm;
+	current_element.offset = offset;
+	
+	signatures_vector.push_back(current_element);
 	offset += blocksize_from_file; 
     }
 
-    free(md4_checksumm_buffer);
- 
     // rollsum, md4 signature, offset
     return true;
 }
